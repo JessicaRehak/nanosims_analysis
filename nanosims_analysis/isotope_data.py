affab18fdbcef7643b7076d83386a3be614b80b5
Joshua Rehak <jsrehak@gmail.com>
2018-09-08 16:08:03 -0700

    initial commit of IsotopeData

---
+++ nanosims_analysis/isotope_data.py
@@ -0,0 +1,14 @@
+"""
+.. module:: isotope_data
+    :synopsis: IsotopeData object, holds all the data for one isotope.
+
+.. moduleauthor:: Joshua Rehak <jsrehak@berkeley.edu>
+
+"""
+
+class IsotopeData():
+    """ An object containing all the data for a single isotope
+    """
+
+    def __init__(self):
+        pass
