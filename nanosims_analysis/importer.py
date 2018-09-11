f83fce9021c356eda17b4cd26128c36433ebc953
Joshua Rehak <jsrehak@gmail.com>
2018-09-11 16:15:33 -0700

    moved file upload to import file

--- nanosims_analysis/importer.py
+++ nanosims_analysis/importer.py
@@ -12,20 +12,20 @@ from pathlib import Path
 import sims
 
 class Importer(object):
-    """ Uploads and stores data from a NanoSIMS file and performs deadtime \
-    correction.
-
-    :param filename: Attempts to open this file to import data.
-    :type filename: string
-
+    """ Importer object for importing data from a NanoSIMS file.
     """
-    def __init__(self, filename):
+    def __init__(self):
         self._isotopes = {}
-        self._filename = filename
-        
-        # Import data
-        self._import_data()
 
+    def add_isotope(self, isotope_data):
+        """ Directly add an IsotopeData object to the importer
+
+        :param isotope_data: IsotopeData object to add.
+        :type isotope_data: IsotopeData
+        """
+        self._isotopes.update({
+            isotope_data.get_label() : isotope_data})
+    
     def get_isotope(self, label):
         """ Get the isotope identified by label
 
@@ -33,29 +33,16 @@ class Importer(object):
         :type label: string
         """
         return self._isotopes[label]
+
+    def import_file(self, filename):
+        """ Uploads and stores data from a NanoSIMS file.
         
-    def perform_deadtime_correction(self, dead_time):
-        """ Performs deadtime correction on each data set. Dwell time can be \
-        found in the header for the NanoSIMS file but dead time must be given.
+        :param filename: Attempts to open this file to import data.
+        :type filename: string
 
-        :param dead_time: Dead time in **seconds**.
-        :type dead_time: float
         """
-        self._dwell_time = float(
-            self._sims_object.header["BFields"][0]["time per pixel"])
-        self._dead_time = dead_time
-
-        for label, isotope in self._isotopes.items():
-            isotope.perform_deadtime_correction(self._dwell_time, self._dead_time)
-
-    def __str__(self):
-        return_string = "Importer object\nImported file: " + self._filename + "\n";
-        return_string += "Isotopes:\n"
-        for label, data in self._isotopes.items():
-            return_string += "  " + str(data) + "\n"
-        return return_string[:-1]
-
-    def _import_data(self):
+        self._filename = filename
+        
         # Verify path exists
         if not Path(self._filename).is_file():
             raise RuntimeError('Bad filename')
@@ -69,3 +56,30 @@ class Importer(object):
             self._isotopes.update({
                 label:
                 IsotopeData(label, isotope_data)})
+
+        
+    def deadtime_correct_all(self, dead_time, dwell_time=0):
+        """ Performs deadtime correction on each data set. Dwell time can usually be \
+        found in the header for the NanoSIMS file but dead time must be given. \
+        See :meth:`~nanosims_analysis.data_structures.IsotopeData.perform_deadtime_correction`.
+
+        :param dead_time: Dead time in **seconds**.
+        :type dead_time: float
+        """
+        if not dwell_time:
+            self._dwell_time = float(
+                self._sims_object.header["BFields"][0]["time per pixel"])
+        else:
+            self._dwell_time = dwell_time
+        self._dead_time = dead_time
+
+        for label, isotope in self._isotopes.items():
+            isotope.perform_deadtime_correction(dwell_time = self._dwell_time,
+                                                dead_time = self._dead_time)
+
+    def __str__(self):
+        return_string = "Importer object\nImported file: " + self._filename + "\n";
+        return_string += "Isotopes:\n"
+        for label, data in self._isotopes.items():
+            return_string += str(data) + "\n"
+        return return_string[:-1]
