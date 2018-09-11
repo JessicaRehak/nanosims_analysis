c95660bc716086fe8edc1cb9af44a46289903f86
Joshua Rehak <jsrehak@gmail.com>
2018-09-11 12:52:16 -0700

    added deadtime correction

--- nanosims_analysis/data_structures.py
+++ nanosims_analysis/data_structures.py
@@ -7,21 +7,49 @@
 
 """
 
+import numpy as np
+
 class IsotopeData(object):
     """ Create an IsotopeData file for an isotope with given name, and data.
     
-    :param isotope_name: Name of the isotope.
-    :type isotope_name: string
+    :param isotope_label: Name of the isotope.
+    :type isotope_label: string
 
     :param isotope_data: The data for the given isotope.
     :type isotope_data: 3D `numpy` array
     """
 
-    def __init__(self, isotope_name, isotope_data):
-        self._isotope_name = isotope_name
-        self._isotope_data = isotope_data
+    def __init__(self, isotope_label, isotope_data):
+        self._label = isotope_label
+        self._data = isotope_data
 
+    def perform_deadtime_correction(self, dwell_time, dead_time):
+        r""" Perform deadtime correction on the count data:\
+
+        .. math:: n=\frac{n_0}{1-n_0\tau}
+        
+        using dead time, :math:`\tau`, and :math:`n_0` is in counts per second:\
+
+        .. math:: n_0=\frac{n}{T}
+
+        where :math:`n` is the counts, and :math:`T` is the dwell time.
+
+        .. important::  Dwell time and dead time may be in any unit of time as \
+           long as they are in the *same* units.
+        
+        :param dwell_time: Dwell time.
+        :type dwell_time: float
+
+        :param dead_time: Dead time.
+        :type dead_time: float
+        """
+        # Get count rates
+        count_rate = np.divide(self._data, dwell_time)
+        self._data = np.divide(count_rate,
+                               1 - np.multiply(count_rate, dead_time))
+        self._label += " (deadtime corrected)"
+        
     def __str__(self):
-        return_string = "Isotope: " + self._isotope_name + "; "
-        return_string += "Data size: " + str(np.shape(self._isotope_data))
-        return return_string        
+        return_string = "label: " + self._label + "; "
+        return_string += "Data size: " + str(np.shape(self._data))
+        return return_string
--- nanosims_analysis/importer.py
+++ nanosims_analysis/importer.py
@@ -18,16 +18,42 @@ class Importer(object):
     :param filename: Attempts to open this file to import data.
     :type filename: string
 
-    :param deadtime_correction: If True (default), performs deadtime correction on all
-                     imported data.
-    :type deadtime_correction: bool
     """
-    def __init__(self, filename, deadtime_correction = True):
-        self._isotope_data = {}
+    def __init__(self, filename):
+        self._isotopes = {}
         self._filename = filename
         
         # Import data
         self._import_data()
+
+    def get_isotope(self, label):
+        """ Get the isotope identified by label
+
+        :param label: isotope label
+        :type label: string
+        """
+        return self._isotopes[label]
+        
+    def perform_deadtime_correction(self, dead_time):
+        """ Performs deadtime correction on each data set. Dwell time can be \
+        found in the header for the NanoSIMS file but dead time must be given.
+
+        :param dead_time: Dead time in **seconds**.
+        :type dead_time: float
+        """
+        self._dwell_time = float(
+            self._sims_object.header["BFields"][0]["time per pixel"])
+        self._dead_time = dead_time
+
+        for label, isotope in self._isotopes.items():
+            isotope.perform_deadtime_correction(self._dwell_time, self._dead_time)
+
+    def __str__(self):
+        return_string = "Importer object\nImported file: " + self._filename + "\n";
+        return_string += "Isotopes:\n"
+        for label, data in self._isotopes.items():
+            return_string += "  " + str(data) + "\n"
+        return return_string[:-1]
 
     def _import_data(self):
         # Verify path exists
@@ -40,6 +66,6 @@ class Importer(object):
         for i, isotope_data in enumerate(self._sims_object.data):
             label = self._sims_object.header["label list"][i]
                       
-            self._isotope_data.update({
+            self._isotopes.update({
                 label:
                 IsotopeData(label, isotope_data)})
