72f68cf11c3e61abd10a0dc466e7dd84f39ec2fc
Joshua Rehak <jsrehak@gmail.com>
2018-09-11 11:15:24 -0700

    removed analyzer, finished moving isotopedata to data_structures

--- nanosims_analysis/data_structures.py
+++ nanosims_analysis/data_structures.py
@@ -8,5 +8,20 @@
 """
 
 class IsotopeData(object):
-    def __init__(self):
-        pass
+    """ Create an IsotopeData file for an isotope with given name, and data.
+    
+    :param isotope_name: Name of the isotope.
+    :type isotope_name: string
+
+    :param isotope_data: The data for the given isotope.
+    :type isotope_data: 3D `numpy` array
+    """
+
+    def __init__(self, isotope_name, isotope_data):
+        self._isotope_name = isotope_name
+        self._isotope_data = isotope_data
+
+    def __str__(self):
+        return_string = "Isotope: " + self._isotope_name + "; "
+        return_string += "Data size: " + str(np.shape(self._isotope_data))
+        return return_string        
