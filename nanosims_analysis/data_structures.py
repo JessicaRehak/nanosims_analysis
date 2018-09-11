ca81cff74cd3c0e00b8769f9b0e342195b26fc0f
Joshua Rehak <jsrehak@gmail.com>
2018-09-11 16:18:30 -0700

    added plotting and gt, lt masking

--- nanosims_analysis/data_structures.py
+++ nanosims_analysis/data_structures.py
@@ -8,6 +8,9 @@
 """
 
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class IsotopeData(object):
     """ Create an IsotopeData file for an isotope with given name, and data.
@@ -22,7 +25,17 @@ class IsotopeData(object):
     def __init__(self, isotope_label, isotope_data):
         self._label = isotope_label
         self._data = isotope_data
+        self._is_deadtime_corrected = False
 
+    def get_label(self):
+        return self._label
+
+    def __lt__(self, value):
+        return self._data < value
+
+    def __gt__(self, value):
+        return self._data > value
+    
     def perform_deadtime_correction(self, dwell_time, dead_time):
         r""" Perform deadtime correction on the count data:\
 
@@ -43,13 +56,59 @@ class IsotopeData(object):
         :param dead_time: Dead time.
         :type dead_time: float
         """
-        # Get count rates
+        # Prevent applying deadtime correction twice
+        if self._is_deadtime_corrected:
+            raise RuntimeError("Error: Isotope " + self._label +
+                               " is already deadtime corrected")
+
+        print("Deadtime correction: isotope: " + self._label + ";\tdwell_time: " +
+              str(dwell_time) + ";\tdead_time: " + str(dead_time))
+
+        # Perform deadtime correction
         count_rate = np.divide(self._data, dwell_time)
         self._data = np.divide(count_rate,
                                1 - np.multiply(count_rate, dead_time))
-        self._label += " (deadtime corrected)"
+        self._is_deadtime_corrected = True
+
+    def plot(self):
+        x = range(np.shape(self._data)[1])
+        y = range(np.shape(self._data)[2])
+        z_max = np.shape(self._data)[0]
+        X, Y = np.meshgrid(x,y)
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection = '3d')
+        
+        vmin = np.min(self._data)
+        vmax = np.max(self._data)
+        
+        for z in range(z_max):
+            x_y_data = self._data[z, :, :]
+            contour = ax.contourf(X, Y, x_y_data, zdir='z', offset=-z, alpha = 1,
+                                  cmap = "CMRmap", vmin=vmin, vmax=vmax)
+            
+        cbar = figp.colorbar(contour, ax = ax)
+        
+        if self._is_deadtime_corrected:
+            cbar.ax.set_xlabel("Counts/sec")
+        else:
+            cbar.ax.set_xlabel("Counts")
+
+        ax.set_zlim((-z_max,0))
+        ax.set_zlim((-z_max,0))
+        ax.set_xlabel("X (px)")
+        ax.set_ylabel("Y (px)")
+        ax.set_zlabel("Planes")
+        ax.set_aspect('equal')
+        ax.invert_xaxis()
+        ax.view_init(elev=45, azim=45)
+        
+        plt.show()
         
     def __str__(self):
         return_string = "label: " + self._label + "; "
-        return_string += "Data size: " + str(np.shape(self._data))
+        return_string += "\tData size: " + str(np.shape(self._data))
+        return_string += "\n\t Corrections: "
+        if self._is_deadtime_corrected:
+            return_string += "deadtime"
         return return_string
--- tests/data_structures_tests.py
+++ tests/data_structures_tests.py
@@ -1,31 +1,80 @@
 from nose.tools import *
 import numpy as np
 
+from nanosims_analysis.importer import Importer
 from nanosims_analysis.data_structures import IsotopeData
 
 class TestClass:
 
     @classmethod
     def setup_class(cls):
-        test_data = np.array([[[0.92672997, 0.98710138, 0.69649032],
+        cls.test_data = np.array([[[0.92672997, 0.98710138, 0.69649032],
                                [0.13295207, 0.66058395, 0.71824247],
                                [0.39265736, 0.03355262, 0.43079679]],
 
                               [[0.0554341 , 0.59301986, 0.43093438],
                                [0.46924799, 0.50525259, 0.38235398],
                                [0.85084851, 0.63285618, 0.93021085]]])
-        cls.testIsotope = IsotopeData("test", test_data)
+        cls.dwell_time = 0.006
+        cls.dead_time  = 4*np.power(10.0,-9)
+        cls.dt_corrected = np.array([[[154.45509043, 164.51700493, 116.0817739 ],
+                                      [ 22.1586803 , 110.09737349, 119.70713565],
+                                      [ 65.44291046,   5.59210346,  71.79948562]],
 
-    def test_dummy(self):
-        dwell_time = 0.006
-        dead_time  = 4*np.power(10.0,-9)
-        corrected = np.array([[[154.45509043, 164.51700493, 116.0817739 ],
-                               [ 22.1586803 , 110.09737349, 119.70713565],
-                               [ 65.44291046,   5.59210346,  71.79948562]],
+                                     [[  9.23901701,  98.83668241,  71.8224173 ],
+                                      [ 78.2080228 ,  84.20879336,  63.72567958],
+                                      [141.80816544, 105.4760745 , 155.03523781]]])
 
-                              [[  9.23901701,  98.83668241,  71.8224173 ],
-                               [ 78.2080228 ,  84.20879336,  63.72567958],
-                               [141.80816544, 105.4760745 , 155.03523781]]])
-        self.testIsotope.perform_deadtime_correction(dwell_time, dead_time)
-        assert_true(np.allclose(self.testIsotope._data,corrected))
-                                
+    def test_isotope_data_deadtime(self):
+        testIsotope = IsotopeData("test", self.test_data)
+        testIsotope.perform_deadtime_correction(dwell_time = self.dwell_time,
+                                                dead_time = self.dead_time)
+        #print(self.testIsotope._data)
+        assert_true(np.allclose(testIsotope._data,self.dt_corrected))
+
+    @raises(RuntimeError)
+    def test_isotope_data_double_deadtime(self):
+        testIsotope = IsotopeData("test", self.test_data)
+        testIsotope.perform_deadtime_correction(dwell_time = self.dwell_time,
+                                                dead_time = self.dead_time)
+        testIsotope.perform_deadtime_correction(dwell_time = self.dwell_time,
+                                                dead_time = self.dead_time)
+
+    def test_mask_lt(self):
+        testIsotope = IsotopeData("test", self.test_data)
+        testIsotope.perform_deadtime_correction(dwell_time = self.dwell_time,
+                                                dead_time = self.dead_time)
+        y = testIsotope < 100;
+        answer = np.array([[[False, False, False ],
+                            [ True , False, False],
+                            [ True,   True,  True]],
+
+                           [[  True,  True,  True ],
+                            [ True ,  True,  True],
+                            [False, False , False]]])
+        assert_true(np.array_equal(y, answer))
+
+    def test_mask_gt(self):
+        testIsotope = IsotopeData("test", self.test_data)
+        testIsotope.perform_deadtime_correction(dwell_time = self.dwell_time,
+                                                dead_time = self.dead_time)
+        y = testIsotope > 100;
+        answer = np.array([[[True, True, True ],
+                            [ False , True, True],
+                            [ False,   False,  False]],
+
+                           [[  False,  False,  False ],
+                            [ False ,  False,  False],
+                            [True, True , True]]])
+        assert_true(np.array_equal(y, answer))
+        
+        
+    def test_importer_deadtime(self):
+        testIsotope = IsotopeData("test", self.test_data)
+        test_importer = Importer()
+        test_importer.add_isotope(testIsotope)
+        test_importer.deadtime_correct_all(dwell_time = self.dwell_time,
+                                                  dead_time = self.dead_time)
+                                                  
+        assert_true(np.allclose(self.dt_corrected,
+                                test_importer.get_isotope("test")._data))
