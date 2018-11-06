7d9972f4831dca32da28eb5587a6804977b50106
Joshua Rehak <jsrehak@gmail.com>
2018-11-06 10:49:27 -0800

    updated example IDP script to include new functionality, VTK output and data roll

--- example_IDP_script.py
+++ example_IDP_script.py
@@ -31,6 +31,12 @@ if str(trim) == 'y':
     trim_amount = input("Please input number of cycles to trim: ")
     importer.trim_front_all(trim_amount)
 
+# For this dataset, there is one pixel at the END of the x-range and y-range
+# that need to be moved to the front of the data set (this is an issue with the
+# .im file), so we set x_roll and y_roll to 1. Negative values will move that
+# number of pixels from the front to the end, the reverse operation.
+importer.roll_all(x_roll = 1, y_roll = 1)
+    
 # # Assign IsotopeData objects to each isotope
 O16  = importer.get_isotope("16O")
 O17  = importer.get_isotope("17O")
@@ -38,8 +44,6 @@ O18  = importer.get_isotope("18O")
 Si28 = importer.get_isotope("28Si")
 S32  = importer.get_isotope("32S")
 Mg24 = importer.get_isotope("24Mg 16O")
-
-
     
 # Makes a RatioData object
 O18_to_O16 = RatioData("O18 to O16", numerator_isotope = O18,
@@ -47,12 +51,6 @@ O18_to_O16 = RatioData("O18 to O16", numerator_isotope = O18,
 O17_to_O16 = RatioData("O17 to O16", numerator_isotope = O17,
                        denominator_isotope = O16)
 
-
 # Output to a VTK File.
 #
-# For this dataset, there is one pixel at the END of the x-range and y-range
-# that need to be moved to the front of the data set (this is an issue with the
-# .im file), so we set x_roll and y_roll to 1. Negative values will move that
-# number of pixels from the front to the end, the reverse operation.
-#
-O16.to_VTK("./structured", x_roll = 1, y_roll = 1)
+O16.to_VTK("./structured")
